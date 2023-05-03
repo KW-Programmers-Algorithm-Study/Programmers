701b37381bbeb0f86a11a4d0334e3e1d3093a908
EHmin <okchrisok@naver.com>
2023-05-03 11:10:21 +0900

    use hash

--- EHmin/week3/3.1.전화번호 목록.py
+++ EHmin/week3/3.1.전화번호 목록.py
@@ -11,10 +11,24 @@ def prefix(phone_book):
             break
         prefix = phone_book[i]
         nextWord = phone_book[i+1]
-        
-        if prefix == nextWord[:len(prefix)]:
+        # prefix로 할당된 문자가 뒷 문자의 가장 앞에 있는지 확인 
+        if prefix == nextWord[:len(prefix)]: 
             return False
     return True 
 
+#-------------------------------- Hash 특성(검색이 빠름)이용 -----------------------------------------
+def prefix_has(phone_book):
+    dict = {}
+    for phone_num in phone_book: # prefix 가능 후보군을 전부 dict으로 생성 
+        dict[phone_num] = 1
+    for phone_num in phone_book:
+        for i in range(len(phone_num)):
+            if phone_num[:i] in dict: # dict셔너리에의 검색이 list에서의 검색보다 빠름 
+                return False
+    return True
+# 검색기능을 활용하고 싶은경우, dict 구조로 하는 것이 훨씬 효율적일 수 있다. >> 리스트로 하는 경우 통과 하지 못함.
+# 무엇을 key로 할것인가 >> unique 한것// 검색하고 싶은것 
+#---------------------------------------------------------------------------------
+
 phone_book = ["119", "97674223", "1195524421"]
-print(phone_book) # 
+print(prefix_has(phone_book)) # 
