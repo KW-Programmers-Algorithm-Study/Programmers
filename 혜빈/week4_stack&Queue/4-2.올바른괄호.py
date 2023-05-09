d5a1e13ca01c22e6f0ef24f14005801a1dd9a9d8
ne0n9uy <2leebins@naver.com>
2023-05-09 22:45:32 +0900

    4-2

--- 혜빈/week4_stack&Queue/4-2.올바른괄호.py
+++ 혜빈/week4_stack&Queue/4-2.올바른괄호.py
@@ -2,27 +2,29 @@ def solution(s):
     # 1.괄호의 총 개수가 짝수여야 함
     # 2.(와 )의 개수가 같아야 함
     # 3.처음은 (, 마지막은 )여야 함
+
+    ##stack을 통해 풀기
+    # 1. 스택에 쌓기(append)
+    # 2. 스택에 ()가 쌓이면 pop
     answer = True
-    left=0
-    right=0
-    if len(s)%2==1 or s[0]!="(": #홀수 개수 혹은 "("로 시작하지 않으면
+    st=[]
+    for ss in s:
+        if ss=='(':
+            st.append(ss) # '('만 스택에 쌓음
+        else: # ')' 라면
+            if st==[]: #스택이 비어있다면 == 첫번째가 ')'로 시작한다면
+                answer=False 
+                break
+            else: # 요소가 ')'이고, 이미 스택 내에 '('가 있다면 
+                st.pop()   #맨 뒤의 '('을 pop 한다 <- ')'이 '('을 만났을 때 
+    if st!=[]: #'('와 ')'의 짝이 맞아 스택 내 전부가 pop 되어 빈 스택이 됐다면
         answer=False
-    elif len(s)%2==0 and s[0]=="(" : #짝수 개수고, "("로 시작할 때 
-        for ss in s:
-            if ss=="(": #(이 나오면
-                left+=1 # +1
-            elif ss==")": #)이 나오면
-                left-=1  #+1
-                s.pop(ss)
-        # print("left: ",left, "right: ",right)                   
-        if left==0 and s[-1]==")": #(과 )의 개수가 같고, 마지막이 )라면
-            answer=True
-        else: answer=False
+    # print(st)
     print(answer)
     return answer
 
 solution("()()")
 solution("(())()")
-solution(")()(")
-solution("(()(")
+solution(")()(") #F
+solution("(()(") #F
 solution("())(()") #issue 
