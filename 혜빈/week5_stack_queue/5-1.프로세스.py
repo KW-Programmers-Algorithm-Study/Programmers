3a0ff2f30a3e8b2c5661a8e1b06ede7a590375e6
ne0n9uy <2leebins@naver.com>
2023-05-16 01:21:38 +0900

    5-1

--- 혜빈/week5_stack_queue/5-1.프로세스.py
+++ 혜빈/week5_stack_queue/5-1.프로세스.py
@@ -5,41 +5,34 @@
 #3. 만약 그런 프로세스가 없다면 방금 꺼낸 프로세스를 실행합니다.
 #   3.1 한 번 실행한 프로세스는 다시 큐에 넣지 않고 그대로 종료됩니다.
 
-# 배열 내 최대 값을 구한다 => 최대값과 각 요소를 비교한다. => 최대값이 첫번째라면, 다른 배열에 넣고, 원래 배열에서 삭제, cnt+1 => 다시 최대값 구해서 비교 
-# 최대값이 첫번째가 아니라면, 첫번째 요소 삭제 후 배열의 맨뒤로 다시 보냄 , 삭제시마다 cnt + 1  
 def solution(priorities, location):
     answer = 0
     q=[] #중요도 순서대로 담을 큐
     # loc=priorities[location]
-    cnt=0
-    priorities2=[]
-
-    for p in priorities:
-        priorities2.append(p)
         
-    while priorities!=[]: # 우선순위 리스트가 빌 때까지
-        mx=max(priorities) #최대값 매번 초기화
-        if mx == priorities[0]: #첫번째에 최대값이 나오면
-            q.append(priorities.pop(0))
-            cnt+=1
-        else: #최대값이 첫번째 요소가 아니라면
-            priorities.append(priorities.pop(0))
-            cnt+=1
-        # answer=cnt
-    locList=list(filter(lambda x: q[x]==priorities2[location], range(len(q))))
-    
-    i=1
-    for i in range(len(locList)+1): 
-        # locList[i]=locList[i]
-        # if locList[i]==location: 
-        print("loc: ",q.index(locList[i]))
-    # answer=q.index(priorities2[location])+1
-    print("locList",locList)
-    print("prior2: ",priorities2)
-    print("q: ",q)
-    print("answer: ",answer)
+    while priorities: # 우선순위 리스트가 빌 때까지
+        mx=max(priorities) #최대값 
+        if location==0: #1. location이 0일 때
+            if mx>priorities[0]: #최대값이 중요도 배열의 첫번째 순서보다 크다면
+                priorities.append(priorities.pop(0)) #삭제 후 뒤로 보내서 추가함
+                location=len(priorities)-1 #맨 뒤로 보냈으므로 location 재설정
+            else: #최대값이 중요도 배열의 첫번째 순서와 같다면
+                q.append(priorities.pop(0)) #최대 중요도 삭제 후 추가
+                answer+=1 #순서 +1(0부터 시작하므로)
+                break #반복문 탈출
+        else: #location이 0이 아니라면(순서가 궁금한 요소가 뒤에 있다면)
+            if mx>priorities[0]: #최대값이 첫번째 요소보다 클 때
+                priorities.append(priorities.pop(0)) #첫번째 요소를 뒤로 보냄
+                location-=1 #앞으로 하나씩 당겨지기 때문에 -1
+            else:  #최대값이 첫번째 요소와 같을 때
+                q.append(priorities.pop(0)) #q 배열에 추가하고
+                location-=1 #앞에서 한개씩 당겨지므로
+                answer+=1 #순서 +1
+                 
+    # print("q: ",q)
+    # print("answer: ",answer)
     return answer
 
 solution([3,1,4,2],2) #1 
-# solution([3,1,4,2],3) #3 1423 4231 231 312 12 21 1 q:4321 
-# solution([1,1,9,1,1,1],0) #5
+solution([3,1,4,2],3) #3 1423 4231 231 312 12 21 1 q:4321 
+solution([1,1,9,1,1,1],0) #5
