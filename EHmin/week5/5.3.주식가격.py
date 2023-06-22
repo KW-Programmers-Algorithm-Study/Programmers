209a54f0b7fee4f85fb720bfcf1321c8ccc3d428
EHmin <okchrisok@naver.com>
2023-06-23 00:20:45 +0900

    5.3 complete

--- EHmin/week5/5.3.주식가격.py
+++ EHmin/week5/5.3.주식가격.py
@@ -2,7 +2,7 @@ import numpy as np
 from collections import deque
 
 def solution(prices):
-    answer = []
+    # answer = []
     # for i in range(len(prices)):
     #     count = 0
     #     for j in prices[i+1:]:
@@ -10,6 +10,7 @@ def solution(prices):
     #         if j < prices[i]:
     #             break
     #     answer.append(count)
+    
     queue = deque(prices)
     answer = []
     
