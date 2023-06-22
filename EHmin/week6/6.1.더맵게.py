7a6b3adaa30dc997397c446b28932f0c5f10086e
EHmin <okchrisok@naver.com>
2023-06-23 00:43:47 +0900

    6.1 clear

---
+++ EHmin/week6/6.1.더맵게.py
@@ -0,0 +1,18 @@
+from heapq import *
+
+def solution(scoville, K):
+    heap = scoville
+    heapify(heap)
+    count = 0
+    while (heap[0] < K) and len(heap) > 1:
+        smallest = heappop(heap)
+        smallest += heappop(heap) * 2
+        heappush(heap, smallest)
+        count += 1
+    return count
+
+
+scoville = [1, 2, 3, 9, 10, 12]
+K = 7
+
+print(solution(scoville, K))
