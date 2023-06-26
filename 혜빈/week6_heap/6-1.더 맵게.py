ca0304d67a151fa199d76247c5c4d1ac3ff98bc4
HyeBeen <86273626+ne0n9uy@users.noreply.github.com>
2023-06-26 18:59:23 +0900

    6-1

--- 혜빈/week6_heap/6-1.더 맵게.py
+++ 혜빈/week6_heap/6-1.더 맵게.py
@@ -1,30 +1,33 @@
 import heapq as hq
 
 import heapq as hq
-
+# 남은 게 1개일 때 : K보다 클 때- 끝. 작을 때- -1 반환
+# 남은 게 2개 이상일 때: K보다 클 때-끝. 작을 때 - 다시 while문 loop
+# 
 def solution(scoville, K):
     answer = 0
     hq.heapify(scoville)
     
-    #남은 요소가 2개부터일 때
+    #남은 요소가 2개 이상일 때
     while len(scoville)>1: #IndexError & h2까지 구해야 하므로
         h1=hq.heappop(scoville) #첫번째로 작은 요소
         
-        if h1> K:
+        if h1>= K:
             # break로 했을 땐 효율성 테스트 통과 x + 정답률 더 낮
             print(answer)
             return answer
-        
         else:
             h2=hq.heappop(scoville) #두번째로 작은 요소
             hq.heappush(scoville,h1+h2*2)
             answer+=1
-    #남은 요소가 1개이고 K보다 작을 때
-    if hq.heappop(scoville) < K:
-        answer=-1
-        print(answer)
-        print(scoville)
+
+    #남은 요소가 1개일 때
+    if scoville[0] >= K:
         return answer
-    
+    else:
+        answer=-1
+    print(answer)
+    print(scoville)
+    return answer
 
 solution([1,2,3,9,10,12],7) #2
